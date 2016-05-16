80b010afe7cf74ce1b61c4f79155fff76016fbdc
dchao <dchao@caltech.edu>
2016-05-15 19:08:21 -0700

    Test declare cursor.

--- test_pq.cc
+++ test_pq.cc
@@ -3,25 +3,52 @@
 
 #include <libpq-fe.h>
 
+static void exit_nicely(PGconn *conn) {
+  PQfinish(conn);
+  exit(1);
+}
+
 int main() {
+
   PGconn *conn = PQconnectdb("dbname=testing");
-
-  std::string query_stmt = 
-    "SELECT mclen, mclund FROM framework_ntuples;";
-
-  std::string s;
-
-  PGresult *res = PQexec(conn, query_stmt.c_str());
-  for (size_t i = 0; i < PQntuples(res); i++) {
-    for (size_t j = 0; j < PQnfields(res); j++) {
-      s = PQgetvalue(res, i, j);
-      std::cout << s << " ";
-      //printf("%-15s", PQgetvalue(res, i, j));
-    }
-    std::cout << std::endl;
+  if (PQstatus(conn) != CONNECTION_OK) {
+    std::cerr << "Connection to database failed: ";
+    std::cerr << PQerrorMessage(conn) << std::endl;
+    exit_nicely(conn);
   }
+
+  PGresult *res = PQexec(conn, "BEGIN");
+  PQclear(res);
+
+  std::string select_stmt = "SELECT mclen, mclund FROM framework_ntuples;";
+
+  res = PQexec(conn, 
+      ("DECLARE myportal CURSOR FOR " + select_stmt).c_str());
+  PQclear(res);
+
+  res = PQexec(conn, "FETCH FORWARD 2 in myportal;");
+
+  for (size_t i = 0; i < PQntuples(res); i++) {
+    std::cout << PQgetvalue(res, i, 0) << std::endl;
+  }
+
+  PQclear(res);
+
+  res = PQexec(conn, "FETCH FORWARD 2 in myportal;");
+
+  for (size_t i = 0; i < PQntuples(res); i++) {
+    std::cout << PQgetvalue(res, i, 0) << std::endl;
+  }
+
+  PQclear(res);
+
+  res = PQexec(conn, "CLOSE myportal;");
+  PQclear(res);
+
+  res = PQexec(conn, "END");
   PQclear(res);
 
   PQfinish(conn);
+
   return 0;
 }
