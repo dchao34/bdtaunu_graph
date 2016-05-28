038e744ea7aa5fce5350bbe09a42944534df2084
dchao <dchao@caltech.edu>
2016-05-28 16:25:42 -0700

    Added configuration file for job tuning.

--- graph_extraction/extract_mcgraph.cc
+++ graph_extraction/extract_mcgraph.cc
@@ -3,22 +3,114 @@
 #include <string>
 #include <vector>
 
+#include <boost/program_options.hpp>
+
 #include "pgstring_convert.h"
 #include "PsqlReader.h"
 
-int main() {
+namespace po = boost::program_options;
 
+void extract_mcgraph(const po::variables_map &vm);
+
+int main(int argc, char **argv) {
+
+  try {
+
+    // define program options
+    po::options_description generic("Generic options");
+    generic.add_options()
+        ("help,h", "produce help message")
+    ;
+
+    po::options_description config("Configuration options");
+    config.add_options()
+        ("dbname", po::value<std::string>(), 
+             "database name. ")
+        ("table_name", po::value<std::string>(), 
+             "name of the table to extract graph information. ")
+        ("output_fname", po::value<std::string>(), 
+             "output csv file name to store extracted result. ")
+        ("cursor_fetch_size", po::value<int>()->default_value(5000), 
+             "number of rows per cursor fetch. ")
+    ;
+
+    po::options_description hidden("Hidden options");
+    hidden.add_options()
+        ("config_file", po::value<std::string>(), 
+             "name of a configuration file. ")
+    ;
+
+    po::options_description cmdline_options;
+    cmdline_options.add(generic).add(config).add(hidden);
+
+    po::options_description config_file_options;
+    config_file_options.add(config);
+
+    po::options_description visible;
+    visible.add(generic).add(config);
+
+    po::positional_options_description p;
+    p.add("config_file", -1);
+
+    // parse program options and configuration file
+    po::variables_map vm;
+    store(po::command_line_parser(argc, argv).
+          options(cmdline_options).positional(p).run(), vm);
+    notify(vm);
+
+    if (vm.count("help") || !vm.count("config_file")) {
+      std::cout << std::endl;
+      std::cout << "Usage: ./extract_mcgraph ";
+      std::cout << "[options] config_fname" << std::endl;
+      std::cout << visible << "\n";
+      return 0;
+    }
+
+    std::ifstream fin(vm["config_file"].as<std::string>());
+    if (!fin) {
+      std::cout << "cannot open config file: ";
+      std::cout << vm["config_file"].as<std::string>() << std::endl;
+      return 0;
+    }
+
+    store(parse_config_file(fin, config_file_options), vm);
+    notify(vm);
+
+    // main routine
+    extract_mcgraph(vm);
+
+  } catch(std::exception& e) {
+
+    std::cerr << "error: " << e.what() << "\n";
+    return 1;
+
+  } catch(...) {
+
+    std::cerr << "Exception of unknown type!\n";
+    return 1;
+  }
+
+  return 0;
+}
+
+void extract_mcgraph(const po::variables_map &vm) {
   
   // open database connection
+  std::string dbname = vm["dbname"].as<std::string>();
+  std::string table_name = vm["table_name"].as<std::string>();
+  int cursor_fetch_size = vm["cursor_fetch_size"].as<int>();
+
   PsqlReader psql;
-  psql.open_connection("dbname=testing");
-  psql.open_cursor(
-      "framework_ntuples", 
-      { "eid", "mclen", "daulen", "dauidx", "mclund" }, 5000);
+  psql.open_connection("dbname=" + dbname);
+  psql.open_cursor(table_name,
+      { "eid", "mclen", "daulen", "dauidx", "mclund" }, 
+      cursor_fetch_size);
 
   // open output file and write title line
-  std::ofstream fout; fout.open("mcgraph_adjacency.csv");
-  fout << "eid,n_vertices,n_edges,from_vertices,to_vertices,lund_id" << std::endl;
+  std::string output_fname = vm["output_fname"].as<std::string>();
+  std::ofstream fout; fout.open(output_fname);
+  fout << "eid,n_vertices,n_edges,";
+  fout << "from_vertices,to_vertices,lund_id" << std::endl;
 
   int eid;
   int mclen;
@@ -69,5 +161,4 @@ int main() {
 
   std::cout << "processed " << n_records << " rows. " << std::endl;
 
-  return 0;
 }
--- graph_extraction/extract_recograph.cc
+++ graph_extraction/extract_recograph.cc
@@ -10,14 +10,19 @@
 #include <algorithm>
 #include <numeric>
 
+#include <boost/program_options.hpp>
+
 #include "pgstring_convert.h"
 #include "PsqlReader.h"
 #include "RecoIndexer.h"
 #include "RecoEdgeAssociator.h"
 
+namespace po = boost::program_options;
+
 using namespace std;
 using namespace boost;
 
+void extract_recograph(const po::variables_map &vm);
 
 // convenience function to determine reconstruction adjacencies 
 // for given block
@@ -81,10 +86,91 @@ void write_record_line(
   os << std::endl;
 }
 
+int main(int argc, char **argv) {
+
+  try {
+
+    // define program options
+    po::options_description generic("Generic options");
+    generic.add_options()
+        ("help,h", "produce help message")
+    ;
+
+    po::options_description config("Configuration options");
+    config.add_options()
+        ("dbname", po::value<std::string>(), 
+             "database name. ")
+        ("table_name", po::value<std::string>(), 
+             "name of the table to extract graph information. ")
+        ("output_fname", po::value<std::string>(), 
+             "output csv file name to store extracted result. ")
+        ("cursor_fetch_size", po::value<int>()->default_value(5000), 
+             "number of rows per cursor fetch. ")
+    ;
+
+    po::options_description hidden("Hidden options");
+    hidden.add_options()
+        ("config_file", po::value<std::string>(), 
+             "name of a configuration file. ")
+    ;
+
+    po::options_description cmdline_options;
+    cmdline_options.add(generic).add(config).add(hidden);
+
+    po::options_description config_file_options;
+    config_file_options.add(config);
+
+    po::options_description visible;
+    visible.add(generic).add(config);
+
+    po::positional_options_description p;
+    p.add("config_file", -1);
+
+    // parse program options and configuration file
+    po::variables_map vm;
+    store(po::command_line_parser(argc, argv).
+          options(cmdline_options).positional(p).run(), vm);
+    notify(vm);
+
+    if (vm.count("help") || !vm.count("config_file")) {
+      std::cout << std::endl;
+      std::cout << "Usage: ./extract_recograph ";
+      std::cout << "[options] config_fname" << std::endl;
+      std::cout << visible << "\n";
+      return 0;
+    }
+
+    std::ifstream fin(vm["config_file"].as<std::string>());
+    if (!fin) {
+      std::cout << "cannot open config file: ";
+      std::cout << vm["config_file"].as<std::string>() << std::endl;
+      return 0;
+    }
+
+    store(parse_config_file(fin, config_file_options), vm);
+    notify(vm);
+
+    // main routine
+    extract_recograph(vm);
+
+  } catch(std::exception& e) {
+
+    std::cerr << "error: " << e.what() << "\n";
+    return 1;
+
+  } catch(...) {
+
+    std::cerr << "Exception of unknown type!\n";
+    return 1;
+  }
+
+  return 0;
+}
+
 
 // reads the framework ntuple reconstruction adjacencies to produce 
 // the more natural adjacency list representation
-int main() {
+void extract_recograph(const po::variables_map &vm) {
 
   // 1. setup data structures. see the BtaTupleMaker block to 
   //    decide how to initialize these structures. 
@@ -139,18 +225,24 @@ int main() {
   std::vector<int> h_reco_idx, l_reco_idx, gamma_reco_idx;
 
   // 3. open output file
-  std::ofstream fout; 
-  fout.open("recograph_adjacency.csv");
+  std::string output_fname = vm["output_fname"].as<std::string>();
+  std::ofstream fout; fout.open(output_fname);
   write_title_line(fout);
 
   // 4. open postgres reader and declare the required input data
+  std::string dbname = vm["dbname"].as<std::string>();
+  std::string table_name = vm["table_name"].as<std::string>();
+  int cursor_fetch_size = vm["cursor_fetch_size"].as<int>();
+
   PsqlReader psql; 
-  psql.open_connection("dbname=testing");
-  psql.open_cursor("framework_ntuples", 
+  psql.open_connection("dbname="+dbname);
+  psql.open_cursor(table_name, 
       { "eid", 
-        "ylund", "blund", "dlund", "clund", "hlund", "llund", "gammalund",
+        "ylund", "blund", "dlund", "clund", 
+        "hlund", "llund", "gammalund",
         "ny", "nb", "nd", "nc", "nh", "nl", "ngamma", 
-        "yndaus", "bndaus", "dndaus", "cndaus", "hndaus", "lndaus", "gammandaus", 
+        "yndaus", "bndaus", "dndaus", "cndaus", 
+        "hndaus", "lndaus", "gammandaus", 
         "yd1lund", "yd2lund", 
         "yd1idx", "yd2idx", 
         "bd1lund", "bd2lund", "bd3lund", "bd4lund", 
@@ -162,7 +254,8 @@ int main() {
         "hd1lund", "hd2lund", 
         "hd1idx", "hd2idx", 
         "ld1lund", "ld2lund", "ld3lund", 
-        "ld1idx", "ld2idx", "ld3idx" });
+        "ld1idx", "ld2idx", "ld3idx" }, 
+      cursor_fetch_size);
 
   // initialize location to save downloaded data
   int eid;
@@ -337,6 +430,5 @@ int main() {
 
   std::cout << "processed " << n_records << " rows. " << std::endl;
 
-  return 0;
 }
 
